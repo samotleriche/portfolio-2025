18dd96d49757da7a1c5ed8e41ffdca26a7781515
samotleriche <samotleriche@gmail.com>
2025-03-15 01:03:49 -0700

    header work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import HomeHero from "@/components/sections/home-hero";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center w-section h-[200vh] pt-[var(--h-navbar)] gap-16 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="items-center justify-items-center pt-[var(--h-navbar)] gap-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex w-section flex-col gap-8 row-start-2 items-start">
+        <HomeHero />
         <HomeHero />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        footer
+      <footer className="bg-cyan-950 h-10 flex items-center justify-center">
+        <p>&copy; Tomas Leriche - {new Date().getFullYear()} </p>
       </footer>
     </div>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
-const navItems = ["Item 1", "Item 2", "Item 3"];
+const navItems = ["About", "Blog", "Contact"];
 
 function Navbar() {
   return (
-    <header className="bg-cyan-900 fixed h-[var(--h-navbar)] top-0 w-full">
-      <div className="w-section h-full">
+    <header className="bg-[rgb(var(--background-dark)/0.75)] backdrop-blur-sm fixed h-[var(--h-navbar)] top-0 w-full border-b border-b-gray-800">
+      <div className="w-section h-full flex justify-between">
+        <div className="flex justify-center items-center">
+          <Link href="/">
+            <p className="text-white text-4xl">TL</p>
+          </Link>
+        </div>
         <ul className="flex gap-3 h-full">
           {navItems.map((item) => (
             <li
               key={item}
-              className="text-white px-3 py-2 flex justify-center items-center"
+              className="text-gray-300 font-semibold px-3 py-2 flex justify-center items-center"
             >
               {item}
             </li>
