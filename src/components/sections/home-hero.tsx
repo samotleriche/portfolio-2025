a415447bbc26b18835158a6024940e34c8b332d7
samotleriche <samotleriche@gmail.com>
2025-03-15 23:00:14 -0700

    update

--- src/components/sections/home-hero.tsx
+++ src/components/sections/home-hero.tsx
@@ -4,12 +4,12 @@ function HomeHero() {
   return (
     <section className="h-section z-20 w-full p-section">
       <div className="flex flex-col gap-4 max-w-[500px]">
-        <h1 className="bg-gradient-to-br w-fit from-teal-400 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent">
-          Greetings <span className="text-5xl">👋</span>
+        <h1 className="bg-gradient-to-br w-fit from-teal-400 to-violet-500 bg-clip-text text-8xl font-extrabold text-transparent">
+          Welcome,
         </h1>
         <p className="text-2xl">
-          I'm Tomas Leriche, software engineer. Welcome my small corner of the
-          internet.
+          I'm Tomas Leriche, software engineer. Welcome to my small corner of
+          the internet.
         </p>
       </div>
     </section>
