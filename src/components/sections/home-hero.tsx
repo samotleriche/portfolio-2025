f308c45c3471145e1f8a3f3169a3c9ee3c1aefd8
samotleriche <samotleriche@gmail.com>
2025-03-15 22:52:34 -0700

    test

--- src/components/sections/home-hero.tsx
+++ src/components/sections/home-hero.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 function HomeHero() {
   return (
-    <div className="h-section">
-      <h1>Coming Soon</h1>
-    </div>
+    <section className="h-section z-20 w-full p-section">
+      <div className="flex flex-col gap-4 max-w-[500px]">
+        <h1 className="bg-gradient-to-br w-fit from-teal-400 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent">
+          Greetings <span className="text-5xl">👋</span>
+        </h1>
+        <p className="text-2xl">
+          I'm Tomas Leriche, software engineer. Welcome my small corner of the
+          internet.
+        </p>
+      </div>
+    </section>
   );
 }
 
