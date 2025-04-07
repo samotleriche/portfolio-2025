e2bc687ad08e95a7770af1e7d0e66d274c5ed429
samotleriche <samotleriche@gmail.com>
2025-04-07 00:07:16 -0700

    hero updates" g" "

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomeHero from "@/components/sections/home-hero";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center pt-[var(--h-navbar)] gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="items-center relative justify-items-center pt-[var(--h-navbar)] gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex w-section flex-col gap-8 row-start-2 items-start">
         <HomeHero />
       </main>
--- src/components/sections/home-hero.tsx
+++ src/components/sections/home-hero.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Spline from "@splinetool/react-spline/next";
 import Button from "../ui/Button";
 
 function HomeHero() {
   return (
-    <section className="h-section z-20 w-full p-section">
+    <section className="h-section flex flex-col gap-5 z-20 w-full p-section">
       <div className="flex flex-col justify-center gap-4 max-w-[500px]">
         <h1 className="bg-gradient-to-bl w-fit from-teal-400 to-violet-500 bg-clip-text text-5xl  md:text-8xl font-extrabold tracking-tight text-transparent">
           Welcome,
@@ -17,6 +18,9 @@ function HomeHero() {
           <Button text="About me" href="/about" />
           <Button text="Latest blog post" href="/blog" />
         </div>
+      </div>
+      <div className="lg:absolute w-full lg:w-[50%] right-0 bottom-0 lg:bottom-auto">
+        <Spline scene="https://prod.spline.design/bh4SaWJT-4e2JTXB/scene.splinecode" />
       </div>
     </section>
   );
